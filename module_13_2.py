fe9d74ad95b5d1d3de26a8a20be2cbd7e9403d54
dmipor333 <dmipor333@ya.ru>
2024-10-14 23:34:27 +0300

    Add files via upload

---
+++ module_13_2.py
@@ -0,0 +1,40 @@
+'''Задача "Бот поддержки (Начало)":
+К коду из подготовительного видео напишите две асинхронные функции:
+start(message) - печатает строку в консоли 'Привет! Я бот помогающий твоему здоровью.'
+. Запускается только когда написана команда '/start' в чате с ботом.
+ (используйте соответствующий декоратор)
+all_massages(message) - печатает строку в консоли 'Введите команду /start,
+чтобы начать общение.'.
+Запускается при любом обращении не описанном ранее. (используйте соответствующий декоратор)
+Запустите ваш Telegram-бот и проверьте его на работоспособность.
+Пример результата выполнения программы:
+Ввод в чат Telegram:
+Хэй!
+/start
+Вывод в консоль:
+Updates were skipped successfully.
+Введите команду /start, чтобы начать общение.
+Привет! Я бот помогающий твоему здоровью.
+
+Примечания:
+Для ответа на сообщение используйте декоратор message_handler.
+При отправке вашего кода на GitHub не забудьте убрать ключ для подключения к вашему боту!'''
+
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+from aiogram.utils import executor
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+@dp.message_handler()
+async def all_message(message):
+    print('Введите команду /start, чтобы начать общение.')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
